999e9c91f15a5b5be45af9576bb9ccd9cfb140e9
Supattra Saekow <64847261+bellsays@users.noreply.github.com>
2024-02-25 14:06:53 +0700

    Add files via upload

--- ass3-app-66130701722.py
+++ ass3-app-66130701722.py
@@ -1,22 +1,22 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+
 import pickle
+import numpy as np
 from sklearn.linear_model import Perceptron
 
-filename = 'per_model-66130701722.sav'
-model = pickle.load(open(filename, 'rb'))
+model = pickle.load(open('per_model-66130701722.sav','rb'))
 
-st.title('Iris Species Prediction Using Preception')
+st.title("Iris species Prediction using Preceptron")
 
-x1 = st.slider('select Input1',0.0,10.0,0.1)
-x2 = st.slider('select Input2',0.0,10.0,0.1)
-x3 = st.slider('select Input3',0.0,10.0,0.1)
-x4 = st.slider('select Input4',0.0,10.0,0.1)
+x1=st.slider('select Input1',0.0,10.0,3.0)
+x2=st.slider('select Input1',0.0,10.0,5.0)
+x3=st.slider('select Input1',0.0,10.0,4.0)
+x4=st.slider('select Input1',0.0,10.0,7.0)
 
-xnew = np.array([x1,x2,x3,x4]).reshape(1,-1)
+xnew = np.array([[6,3,5,3]])#.reshape(1,-1)
 
-predict = model.predict(xnew)
+xnew
 
-st.write("##Prediction Result")
-st.write('Species',predict[0])
+pred = model.predict(xnew)
+st.write("## Prediction resulte:")
+st.write('Species:',pred[0])
