01f0a0af3f7ba82c1c9c1e269d20abb23f0fdf2a
Supattra Saekow <64847261+bellsays@users.noreply.github.com>
2024-02-26 13:52:57 +0700

    Add files via upload

--- ass3-app-66130701722.py
+++ ass3-app-66130701722.py
@@ -9,9 +9,9 @@ model = pickle.load(open('per_model-66130701722.sav','rb'))
 st.title("Iris species Prediction using Preceptron")
 
 x1 = st.slider('select Input1',0.0,10.0,3.0)
-x2 = st.slider('select Input1',0.0,10.0,5.0)
-x3 = st.slider('select Input1',0.0,10.0,4.0)
-x4 = st.slider('select Input1',0.0,10.0,7.0)
+x2 = st.slider('select Input2',0.0,10.0,5.0)
+x3 = st.slider('select Input3',0.0,10.0,4.0)
+x4 = st.slider('select Input4',0.0,10.0,7.0)
 
 xnew = np.array([[x1,x2,x3,x4]])#.reshape(1,-1)
 
