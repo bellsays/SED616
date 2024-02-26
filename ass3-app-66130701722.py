04cc87c4551728785b6c3dafb30baa889e192ee4
Supattra Saekow <64847261+bellsays@users.noreply.github.com>
2024-02-26 12:45:55 +0700

    Add files via upload

--- ass3-app-66130701722.py
+++ ass3-app-66130701722.py
@@ -4,19 +4,18 @@ import pickle
 import numpy as np
 from sklearn.linear_model import Perceptron
 
-model = pickle.load(open('per_model-66130701722.sav','rb'))
+model132 = pickle.load(open('per_model-66130701722.sav','rb'))
 
 st.title("Iris species Prediction using Preceptron")
 
-x1=st.slider('select Input1',0.0,10.0,3.0)
-x2=st.slider('select Input1',0.0,10.0,5.0)
-x3=st.slider('select Input1',0.0,10.0,4.0)
-x4=st.slider('select Input1',0.0,10.0,7.0)
+x1 = st.slider('select Input1',0.0,10.0,3.0)
+x2 = st.slider('select Input2',0.0,10.0,5.0)
+x3 = st.slider('select Input3',0.0,10.0,4.0)
+x4 = st.slider('select Input4',0.0,10.0,7.0)
 
 xnew = np.array([[6,3,5,3]])#.reshape(1,-1)
 
-xnew
 
-pred = model.predict(xnew)
+model132.predict(xnew)
 st.write("## Prediction resulte:")
 st.write('Species:')
