dd13336867ba2508feb76ad88aa69b622b77f824
Supattra Saekow <64847261+bellsays@users.noreply.github.com>
2024-02-26 13:47:27 +0700

    Add files via upload

--- ass3-app-66130701722.py
+++ ass3-app-66130701722.py
@@ -3,19 +3,18 @@ import streamlit as st
 import pickle
 import numpy as np
 from sklearn.linear_model import Perceptron
-model_aap_per = Perceptron(tol=1e-3, random_state=0)
-model_aap_per = pickle.load(open('per_model-66130701722.sav','rb'))
+
+model = pickle.load(open('per_model-66130701722.sav','rb'))
 
 st.title("Iris species Prediction using Preceptron")
 
 x1 = st.slider('select Input1',0.0,10.0,3.0)
-x2 = st.slider('select Input2',0.0,10.0,5.0)
-x3 = st.slider('select Input3',0.0,10.0,4.0)
-x4 = st.slider('select Input4',0.0,10.0,7.0)
+x2 = st.slider('select Input1',0.0,10.0,5.0)
+x3 = st.slider('select Input1',0.0,10.0,4.0)
+x4 = st.slider('select Input1',0.0,10.0,7.0)
 
-xnew = np.array([[6,3,5,3]])#.reshape(1,-1)
+xnew = np.array([[x1,x2,x3,x4]])#.reshape(1,-1)
 
-
-model_aap_per.predict(xnew)
+pred = model.predict(xnew)
 st.write("## Prediction resulte:")
-st.write('Species:')
+st.write('Species:', pred[0])
